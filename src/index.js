8d8366e0b259ca9b2cae1077776a700cbb7661f0
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-01-09 23:06:07 -0500

    Create Highlights Component that contains Highlight component

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes, faBolt, faTags, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart, faTimes, faBolt, faTags, faStar, faStarHalfAlt, faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faTags, faStar, faStarHalfAlt);
+library.add(faBars, faShoppingCart, faTimes, faBolt, faTags, faStar, faStarHalfAlt, faRecordVinyl);
 
 ReactDOM.render(
   <React.StrictMode>

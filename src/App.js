8acf371c6f5ced1702476866c48ffc71e6da03e2
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-01-11 12:49:36 -0500

    Setup React Router Dom and create first route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import Nav from "./components/Nav";
-import Landing from "./components/Landing";
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from "./pages/Home";
 
 function App() {
   return (
+    <Router >
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </div>
+    </Router>
   );
 }
 

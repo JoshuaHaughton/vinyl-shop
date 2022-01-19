cb95fb62db84d7122617ed7d25a23814dcde8c44
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-01-19 11:35:20 -0500

    Fix alignment of back-arrow on vinylInfo page, and improved css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import HeroImage from '../assets/Hero.png';
+import HeroImage from '../assets/Hero2.svg';
 
 
 const Landing = () => {
@@ -8,15 +8,15 @@ const Landing = () => {
     <section id="landing">
       <header>
         <div className="header__container">
-          <figure className="header__img--wrapper">
-            <img src={HeroImage} alt="" />
-          </figure>
           <div className="header__description">
             <h1>Canada's top rated online vinyl exchange!</h1>
             <h2>Find your dream vinyl with <span className="blue">Vinyl Fresh</span></h2>
             <Link to="/vinyls">
               <button className="btn">Browse Vinyls</button>
             </Link>
+            <figure className="header__img--wrapper">
+            <img src={HeroImage} alt="" />
+          </figure>
           </div>
         </div>
       </header>

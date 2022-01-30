fcf50dcd34dacf54a80695cfb5c2313d22c2929c
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-01-30 12:48:02 -0500

    Close menu when link is clicked in mobile overlay menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import logo from '../assets/Logo.svg'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from "../assets/Logo.svg";
+import { Link, Navigate } from "react-router-dom";
 
 const Nav = ({ numberOfItems }) => {
   const openMenu = () => {
     document.body.classList += " menu--open";
-  }
+  };
 
   const closeMenu = () => {
     document.body.classList.remove("menu--open");
-  }
+  };
+
+  const turnOff = (place) => {
+    <Navigate to={place} />;
+    closeMenu();
+  };
 
   return (
     <nav>
       <div className="nav__container">
         <Link to="/">
-          <img src={logo} alt="" className="logo"/>
+          <img src={logo} alt="" className="logo" />
         </Link>
         <ul className="nav__links">
           <li className="nav__list">
@@ -34,29 +39,29 @@ const Nav = ({ numberOfItems }) => {
           </button>
           <li className="nav__icon">
             <Link to="/cart" className="nav__link">
-              <FontAwesomeIcon icon="shopping-cart"/>
+              <FontAwesomeIcon icon="shopping-cart" />
             </Link>
-            {
-              numberOfItems > 0 && <span className="cart__length">{numberOfItems}</span>
-            }
+            {numberOfItems > 0 && (
+              <span className="cart__length">{numberOfItems}</span>
+            )}
           </li>
         </ul>
         <div className="menu__backdrop">
           <button className="btn__menu btn__menu--close" onClick={closeMenu}>
-            <FontAwesomeIcon icon="times"/>
+            <FontAwesomeIcon icon="times" />
           </button>
           <ul className="menu__links">
-            <li className="menu__list">
+            <li className="menu__list" onClick={closeMenu}>
               <Link to="/" className="menu__link">
                 Home
               </Link>
             </li>
-            <li className="menu__list">
+            <li className="menu__list" onClick={closeMenu}>
               <Link to="/vinyls" className="menu__link">
                 Vinyls
               </Link>
             </li>
-            <li className="menu__list">
+            <li className="menu__list" onClick={closeMenu}>
               <Link to="/cart" className="menu__link">
                 Cart
               </Link>
@@ -65,7 +70,7 @@ const Nav = ({ numberOfItems }) => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;

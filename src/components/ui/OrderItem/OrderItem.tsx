e37dc61d0f7dbd557995f707de963a475c744c7c
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-04-30 09:55:04 -0400

    Render a skeleton loading image for OrderItems

--- src/components/ui/OrderItem/OrderItem.tsx
+++ src/components/ui/OrderItem/OrderItem.tsx
@@ -1,18 +1,35 @@
 import { useNavigate } from "react-router-dom";
 import classes from "./OrderItem.module.css";
 import { OrderItemType } from "../../../types";
+import { useEffect, useState } from "react";
+import skeleton from "../../../assets/skeleton.png";
 
 const OrderItem = ({ item }: OrderItemType) => {
+  const [img, setImg] = useState<HTMLImageElement>();
+
   const navigate = useNavigate();
 
   const clickHandler = () => {
     navigate(`/vinyls/${item.id}`);
   };
 
+  useEffect(() => {
+    let image = new Image()!;
+    if (item.url) {
+      image.src = item.url || "null";
+
+      image.onload = () => {
+        if (item.url) {
+          setImg(image);
+        }
+      };
+    }
+  }, []);
+
   return (
     <div className={classes.orderItem}>
       <figure className={classes.imgWrapper}>
-        <img src={item.url} alt={`${item.title} Vinyl Cover`} />
+        <img src={img ? img.src : skeleton} alt={`${item.title} Vinyl Cover`} />
       </figure>
       <div className={classes.itemDescription}>
         <h5>{item.title}</h5>
